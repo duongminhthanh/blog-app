7647d91e87f87c294f55eedfda0217510a8be442
duongminhthanh <62210826+duongminhthanh@users.noreply.github.com>
2026-02-03 15:40:09 +0700

    seeding data

--- SyncSyntax/SyncSyntax/Migrations/20260203034218_SeedingInitalData.Designer.cs
+++ SyncSyntax/SyncSyntax/Migrations/20260203034218_SeedingInitalData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SyncSyntax.Data;
 
@@ -11,9 +12,11 @@ using SyncSyntax.Data;
 namespace SyncSyntax.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260203034218_SeedingInitalData")]
+    partial class SeedingInitalData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
