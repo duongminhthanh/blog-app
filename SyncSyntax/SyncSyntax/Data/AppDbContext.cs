6a7f433708f4ea096afb9bea793da69e06c0164a
duongminhthanh <62210826+duongminhthanh@users.noreply.github.com>
2026-02-03 10:34:55 +0700

    configure db context, add migration

---
+++ SyncSyntax/SyncSyntax/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SyncSyntax.Models;
+
+namespace SyncSyntax.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
