7a6f02cc207fa1ae52dd406050d784e674f75b18
duongminhthanh <62210826+duongminhthanh@users.noreply.github.com>
2026-02-03 15:54:46 +0700

    Creating Dropdown Using SelectListItem

---
+++ SyncSyntax/SyncSyntax/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SyncSyntax.Data;
+
+namespace SyncSyntax.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PostController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            var postViewModel = new Models.ViewModels.PostViewModel();
+            postViewModel.Categories = _context.Categories
+                .Select(c =>
+                new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.Name
+                }).ToList();
+            return View(postViewModel);
+        }
+    }
+}
---
+++ SyncSyntax/SyncSyntax/Models/ViewModels/PostViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SyncSyntax.Models.ViewModels
+{
+    public class PostViewModel
+    {
+        public Post Post { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IFormFile? FeatureImage { get; set; }
+    }
+}
